10e22c89d9a940b0e0ec5cc855f20b9068c82210
jeremyfelt <jeremy.felt@gmail.com>
2021-03-24 16:45:49 -0700

    Add new helper functions to ease publishing/credentials

--- R/appendPost.R
+++
@@ -1,8 +0,0 @@
-
-appendToPost =
-function(postid, content, login = getOption("WordpressLogin", stop("need a login and password")))
-{
-  current = getPost(postid, login)
-  new = paste(current, content)
-  editPost(postid, new, login, TRUE)
-}
