8e21a88ad2569e4ab6b3771079704ed98ed287a6
jeremyfelt <jeremy.felt@gmail.com>
2022-02-09 12:43:39 -0800

    Apply linting suggestions for R code via lintr

--- R/rest.R
+++ R/rest.R
@@ -1,35 +1,46 @@
-setRESTCredentials <- function( siteURL, siteUser, sitePass, path = "/wp-json/wp/v2/posts" ) {
-	options(
-		RESTPostURL = glue( "{siteURL}{path}" )
-	)
+set_rest_credentials <- function(
+    site_url,
+    site_user,
+    site_password,
+    path = "/wp-json/wp/v2/posts"
+) {
+    options(
+        RESTPostURL = glue("{site_url}{path}")
+    )
 
-	options(
-		RESTAuthHeader = base64encode(
-	  		charToRaw( glue("{siteUser}:{sitePass}") )
-		)
-	)
+    options(
+        RESTAuthHeader = base64encode(
+              charToRaw(glue("{site_user}:{site_password}"))
+        )
+    )
 }
 
-publishREST <- function( file, postTitle, currentPostId = 0, publishPost = FALSE ) {
-	htmlFile = render(
-		file,
-		output_format='html_fragment'
-	)
+publish_rest <- function(file, post_title, post_id = 0, publish = FALSE) {
+    html_content <- render(
+        file,
+        output_format = "html_fragment"
+    )
 
-	body = list(
-		title = postTitle,
-		content = file_string( htmlFile )
-	)
+    body <- list(
+        title = post_title,
+        content = file_string(html_content)
+    )
 
-	authKey = getOption( "RESTAuthHeader", stop( "Set an authentication header with setRESTCredentials" ) )
+    auth_key <- getOption(
+        "RESTAuthHeader",
+        stop("Set an authentication header with set_REST_credentials")
+    )
 
-	POST(
-		getOption( "RESTPostURL", stop( "Set a site URL with setRESTCredentials" ) ),
-		body = body,
-		encode = "json",
-		add_headers(
-			Authorization = glue("Basic {authKey}")
-		),
-		verbose()
-	)
+    POST(
+        getOption(
+            "RESTPostURL",
+            stop("Set a site URL with set_REST_credentials")
+        ),
+        body = body,
+        encode = "json",
+        add_headers(
+            Authorization = glue("Basic {auth_key}")
+        ),
+        verbose()
+    )
 }
